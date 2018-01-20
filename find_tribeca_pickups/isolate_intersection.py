f7409005f0cb8c4409f3bd0afe53c5c0f73ba202
King Crawford <crawboy@me.com>
2018-01-20 17:33:47 -0500

    create skeleton of isolate_intersection module

---
+++ find_tribeca_pickups/isolate_intersection.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+read_file = 'tribeca_data_2016-06.csv'
+
+intersection = np.array([40.720768, -74.010015])
+half_block_from_int = np.array([40.720410, -74.010097])
+
+radius = np.linalg.norm(half_block_from_int - intersection)
